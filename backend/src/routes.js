51c22ece5316e02c33258e9554710bc05786f62e
Diego Chagas <drochachagas@gmail.com>
2019-11-13 05:56:32 -0300

    Install Nodemon and Sucrase to debug application

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,9 @@
-const { Router } = require('express');
+import { Router } from 'express';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-  res.json({ message: 'Hello world!' });
+  res.json({ message: 'Hello Diego!' });
 });
 
-module.exports = routes;
+export default routes;
