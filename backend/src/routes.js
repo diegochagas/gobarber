18bc6991da272af1583fa93074d3063b0e38dcc1
Diego Chagas <drochachagas@gmail.com>
2019-11-15 11:50:29 -0300

    Create User's model and models' loader

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  res.json({ message: 'Hello Diego!' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Diego Chagas',
+    email: 'diego@diego.com',
+    password_hash: '12345678',
+  });
+
+  res.json(user);
 });
 
 export default routes;
