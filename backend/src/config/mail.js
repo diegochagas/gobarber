b99bc1a9ffd58c66b85b2b6c05be4648e5bec1d0
Diego Chagas <drochachagas@gmail.com>
2019-11-24 11:28:19 -0300

    Configure Nodemailer

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '50d6fdb9fd6799',
+    pass: '53c75d70079811',
+  },
+  default: {
+    from: 'GoBarber Team <noreply@gobarber.com>',
+  },
+};
