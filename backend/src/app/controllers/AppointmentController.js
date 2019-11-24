b71d4bf6da788b129c596c5c116681b917ce40b9
Diego Chagas <drochachagas@gmail.com>
2019-11-24 09:59:34 -0300

    User notification list

--- backend/src/app/controllers/AppointmentController.js
+++ backend/src/app/controllers/AppointmentController.js
@@ -1,9 +1,10 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
 
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
   async index(req, res) {
@@ -89,6 +90,19 @@ class AppointmentController {
       user_id: req.userId,
       provider_id,
       date: hourStart,
+    });
+
+    /**
+     * Notify appointment provider
+     */
+
+    const user = await User.findByPk(req.userId);
+
+    const formattedDate = format(hourStart, "MMMM dd', at' H:mm");
+
+    await Notification.create({
+      content: `New scheduling for ${user.name} on ${formattedDate}`,
+      user: provider_id,
     });
 
     return res.json(appointment);
