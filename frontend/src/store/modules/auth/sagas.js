c3d47e032eaaccface24981ffc0d5219a0a05bf1
Diego Chagas <drochachagas@gmail.com>
2019-12-07 07:47:51 -0300

    Register on application

--- frontend/src/store/modules/auth/sagas.js
+++ frontend/src/store/modules/auth/sagas.js
@@ -33,6 +33,23 @@ export function* signIn({ payload }) {
   }
 }
 
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, 'users', {
+      name, email, password, provider: true
+    });
+
+    history.push('/');
+  } catch(err) {
+    toast.error('SignUp fail, verify your fields!');
+
+    yield put(signFailure());
+  }
+}
+
 export default all([
-  takeLatest('@auth/SIGN_IN_REQUEST', signIn)
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
 ]);
