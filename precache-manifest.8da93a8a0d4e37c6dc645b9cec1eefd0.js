078247e98dd5b149f3ae7e638f1faa245df62deb
Diego Chagas <drochachagas@gmail.com>
2019-12-31 08:33:39 -0300

    Updates

---
+++ precache-manifest.8da93a8a0d4e37c6dc645b9cec1eefd0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f740d791bf8496776cb91d06c13a9417",
+    "url": "/gobarber/index.html"
+  },
+  {
+    "revision": "365f31302ae56b0685a7",
+    "url": "/gobarber/static/css/2.07b170e0.chunk.css"
+  },
+  {
+    "revision": "365f31302ae56b0685a7",
+    "url": "/gobarber/static/js/2.8e6c12e3.chunk.js"
+  },
+  {
+    "revision": "ac599af6ea258d0f22d3630cae378530",
+    "url": "/gobarber/static/js/2.8e6c12e3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0f9d1469f41e8d5caa24",
+    "url": "/gobarber/static/js/main.7332c7ee.chunk.js"
+  },
+  {
+    "revision": "d2ad8a30f60fb4deaabc",
+    "url": "/gobarber/static/js/runtime-main.95f48b39.js"
+  },
+  {
+    "revision": "a3c4dca3142c09d9c87ba896542e4034",
+    "url": "/gobarber/static/media/logo-purple.a3c4dca3.svg"
+  },
+  {
+    "revision": "63c71f84c93f9cd2b6afc2f7ae4d9423",
+    "url": "/gobarber/static/media/logo.63c71f84.svg"
+  }
+]);
